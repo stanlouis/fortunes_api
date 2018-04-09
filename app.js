ca63c8ae5769a5f150707d55b4de1fc2ba23fbca
Stanley Louis <stanlouis@msn.com>
2018-04-09 06:13:40 -0400

    Get route for random fortune or a fortune by id

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ app.get('/fortunes', (req, res) => {
     res.json(fortunes);
 });
 
+app.get('/fortunes/random', (req, res) => {
+    res.json(fortunes[Math.floor(Math.random() * fortunes.length)]);
+});
+
+app.get('/fortunes/:id', (req, res) => {
+    console.log(req.params);
+    res.json(fortunes.find(f => f.id == req.params.id));
+});
+
 module.exports = app;
