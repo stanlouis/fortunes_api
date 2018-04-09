f7c5105fadaa2618d487247ebe5aa2baa59ba4bc
Stanley Louis <stanlouis@msn.com>
2018-04-09 08:07:07 -0400

    Update fortunes put method

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ app.get('/fortunes/:id', (req, res) => {
     res.json(fortunes.find(f => f.id == req.params.id));
 });
 
+const writeFortunes = jsonFortunes => {
+    fs.writeFile('./data/fortunes.json', JSON.stringify(jsonFortunes), (err) => {
+        console.log(err);
+    });
+}
+
 app.post('/fortunes', (req, res) => {
     const {
         message,
@@ -34,10 +40,24 @@ app.post('/fortunes', (req, res) => {
         lucky_number,
         spirit_animal
     });
-    fs.writeFile('./data/fortunes.json', JSON.stringify(new_fortunes), (err) => {
-        console.log(err);
-    });
+    
+    writeFortunes(new_fortunes);
+
     res.json(new_fortunes);
+});
+
+app.put('/fortunes/:id', (req, res) => {
+    const { id } = req.params;
+
+    const old_fortune = fortunes.find(f => f.id == id);
+
+    ['message', 'lucky_number', 'spirit_animal'].forEach(key => {
+        if (req.body[key]) old_fortune[key] = req.body[key];
+    });
+
+    writeFortunes(fortunes);
+
+    res.json(fortunes);
 });
 
 module.exports = app;
