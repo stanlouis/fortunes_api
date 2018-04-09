1814506eb5d7e2a2a766adbb71bc1257d38239c8
Stanley Louis <stanlouis@msn.com>
2018-04-09 07:05:55 -0400

    Fortunes Post Method using Postman

--- app.js
+++ app.js
@@ -20,30 +20,24 @@ app.get('/fortunes/:id', (req, res) => {
 });
 
 app.post('/fortunes', (req, res) => {
-    /*curl request*/
-    /*
-    curl -H "Content-Type: application/json" -X POST -d '{"id": 5,"message":
-    "Cultivate a new good habit.","lucky_number": 5,"spirit_animal":
-    "Dog"}' http://localhost:3000/fortunes
-    */
-   console.log(req.body);
+    const {
+        message,
+        lucky_number,
+        spirit_animal
+    } = req.body;
 
-   const {message, lucky_number, spirit_animal } = req.body;
+    const fortunes_ids = fortunes.map(f => f.id);
 
-   const fortunes_ids = fortunes.map( f => f.id);
-
-   const fortune = {
-       id: (fortunes_ids.length > 0 ? Math.max(...fortunes_ids ) : 0) + 1, 
-       message, 
-       lucky_number, 
-       spirit_animal 
-    };
-
-   const new_fortunes = fortunes.concat(fortune);
-   fs.writeFile( './data/fortunes.json', JSON.stringify(new_fortunes), (err) => {
-       console.log(err);
-   });
-   res.json(new_fortunes);
+    const new_fortunes = fortunes.concat({
+        id: (fortunes_ids.length > 0 ? Math.max(...fortunes_ids) : 0) + 1,
+        message,
+        lucky_number,
+        spirit_animal
+    });
+    fs.writeFile('./data/fortunes.json', JSON.stringify(new_fortunes), (err) => {
+        console.log(err);
+    });
+    res.json(new_fortunes);
 });
 
 module.exports = app;
